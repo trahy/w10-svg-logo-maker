d5ef5c885bd66e2c2ec4d4d8e936d97be3041c2e
unknown <trahy032@gmail.com>
2023-08-19 14:32:55 +0930

    added prompts

---
+++ index.js
@@ -0,0 +1,36 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const shapes = require('shapes')
+
+// array of questions for user input
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'Enter a text for the logo (up to three characters)',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'Enter a colour for the text (OR use a hexadecimal number)',
+        name: 'textColour',
+    },
+    {
+        type: 'list',
+        message: 'Select the shape of your logo',
+        choices: ['Circle', 'Triangle', 'Square'],
+        name: 'shape',
+    },
+    {
+        type: 'input',
+        message: 'Enter a colour for the shape (OR use a hexadecimal number)',
+        name: 'shapeColour',
+    },
+
+  ])
+  .then((data) => {
+    const readmeContent = generateMarkdown(data);
+    fs.writeFile('./examples/logo.svg', readmeContent, (err) =>
+    err ? console.error(err) : console.log('The README file has been successfully created!')
+    );
+  });
